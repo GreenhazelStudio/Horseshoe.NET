4e0f2f4eedf3e74c8492dc6bd47a0832de9be527
Carl Thomas <Carl.Thomas@derbyllc.com>
2020-02-13 09:18:21 -0500

    More SSRS tests

--- TestConsole/SsrsTests.cs
+++ TestConsole/SsrsTests.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using Horseshoe.NET.ConsoleX;
+using Horseshoe.NET.IO;
 using Horseshoe.NET.IO.ReportingServices;
 
 namespace TestConsole
@@ -19,7 +22,10 @@ namespace TestConsole
             (
                 new[]
                 {
-                    "Build Urls"
+                    "Build Urls",
+                    "Launch Test Report",
+                    "Launch Report",
+                    "Launch Report w/ '&'",
                 },
                 title: "SSRS Test Menu"
             );
@@ -28,6 +34,27 @@ namespace TestConsole
                 case "Build Urls":
                     Console.WriteLine("File: " + ReportUtil.BuildFileUrl("/Accounting/Annual Report", reportServer: "http://localhost", parameters: new Dictionary<string, object> { { "parm1", "ezstr" }, { "parm2", "cr&zy str" } }));
                     Console.WriteLine("Link: " + ReportUtil.BuildHyperlinkUrl("/Accounting/Annual Report", reportServer: "http://localhost", parameters: new Dictionary<string, object> { { "parm1", "ezstr" }, { "parm2", "cr&zy str" } }));
+                    break;
+                case "Launch Test Report":
+                    Console.WriteLine("Downloading...");
+                    var testReport = ReportServer.Render("/Operations/MustInherit/Sandbox/ParamTest", reportServer: "https://reports.derbyllc.com", parameters: new Dictionary<string, object> { { "param1", "12 34 & 56" } }, targetFileName: "param-test.pdf", targetDirectory: Path.GetTempPath());
+                    Console.WriteLine(FileUtil.GetDisplayFileSize(new FileInfo(testReport)));
+                    Console.WriteLine("Launch...");
+                    Process.Start(testReport);
+                    break;
+                case "Launch Report":
+                    Console.WriteLine("Downloading...");
+                    var report = ReportServer.Render("/Operations/Division 13/Navision Reports/Vendor Planning Schedule Flex (Bulk Email)", reportServer: "https://reports.derbyllc.com", parameters: new Dictionary<string, object> { { "Vendor", "ECIM-13" }, { "Worksheet", "GBGWPL" }, { "CutoffDays", "352" } }, targetFileName: "report.pdf", targetDirectory: Path.GetTempPath());
+                    Console.WriteLine(FileUtil.GetDisplayFileSize(new FileInfo(report)));
+                    Console.WriteLine("Launch...");
+                    Process.Start(report);
+                    break;
+                case "Launch Report w/ '&'":
+                    Console.WriteLine("Downloading...");
+                    var reportwamp = ReportServer.Render("/Operations/Division 13/Navision Reports/Vendor Planning Schedule Flex (Bulk Email)", reportServer: "https://reports.derbyllc.com", parameters: new Dictionary<string, object> { { "Vendor", "E&OTO-13" }, { "Worksheet", "GBGWPL" }, { "CutoffDays", "352" } }, targetFileName: "report.pdf", targetDirectory: Path.GetTempPath());
+                    Console.WriteLine(FileUtil.GetDisplayFileSize(new FileInfo(reportwamp)));
+                    Console.WriteLine("Launch...");
+                    Process.Start(reportwamp);
                     break;
             }
         }
