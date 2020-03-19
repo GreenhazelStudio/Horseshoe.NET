95789815b768e44c52e3c5284e8db7b978f25ccb
Carl Thomas <Carl.Thomas@derbyllc.com>
2020-03-19 18:27:28 -0400

    Added AD tests

---
+++ TestConsole/ADTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Horseshoe.NET;
+using Horseshoe.NET.ActiveDirectory;
+using Horseshoe.NET.ConsoleX;
+using Horseshoe.NET.Text;
+
+namespace TestConsole
+{
+    class ADTests : Routine
+    {
+        public override Title Title => "AD Tests";
+        public override bool Looping => true;
+
+        string[] Menu => new[]
+        {
+            "What is my domain controller?"
+        };
+
+        public override void Do()
+        {
+            Console.WriteLine();
+            var selection = PromptMenu
+            (
+                Menu
+            );
+            RenderListTitle(selection.SelectedItem);
+            switch (selection.SelectedItem)
+            {
+                case "What is my domain controller?":
+                    Console.WriteLine("My domain controller: " + TextUtil.RevealNullOrBlank(ADUtil.DetectDomainController()));
+                    break;
+            }
+        }
+    }
+}
